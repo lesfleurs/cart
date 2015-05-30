97e97785e846b62532c663524c6798fba5e5d188
lesfleurs <sola@kosherkampus.com>
2015-05-30 18:43:39 -0500

    fixed acessor functions in item_shipping_class

--- src/lesfleurs/cart/Item.php
+++ src/lesfleurs/cart/Item.php
@@ -1,35 +1,33 @@
 <?php
 
-namespace lesfleurs\cart 
+namespace lesfleurs\cart;
+
+class Item_shipping
+{
+
+    protected $shipping_provider;
+    protected $shipping_cost;
+    protected $processing_time;
 
 
-class Item_shipping {
+    public function __construct($data = array())
+    {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        }
+    }
 
-	protected $shipping_provider;
-	protected $shipping_cost;
-	protected $processing_time;
-	
-	
-	public function ($data = array()) 
-	{
-		foreach ($data as $key => $value)
-		{
-			$this->$key = $value;
-		}
-	}
-	
-	public function __get($property)
-	{
-  		if (property_exists($this, $property)) 
-  		{
-    		return $this->$property;
-    	}
-  	}    
-   public function __set($property, $value)
-	{
-  		if (property_exists($this, $property))
-  		 {
-    		$this->$property = $value;
-  	}
+    public function __get($property)
+    {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+    }
+    public function __set($property, $value)
+    {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
 
+    }
 }
